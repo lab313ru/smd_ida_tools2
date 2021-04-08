6457e12854b2b4d7449415eb561fb209f8fa3b02
Vladimir Kononovich <newinferno@gmail.com>
2021-04-08 18:26:29 +0300

    Added XP like manifest file.

--- Gens/src/g_main.cpp
+++ Gens/src/g_main.cpp
@@ -6373,3 +6373,7 @@ void LoadFlags(int flags)
     LeftRightEnabled = (flags >> 16) & 1;
     return;
 }
+
+#pragma comment(linker,"\"/manifestdependency:type='win32' \
+name='Microsoft.Windows.Common-Controls' version='6.0.0.0' \
+processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
