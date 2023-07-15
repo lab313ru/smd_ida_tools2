c8c08be895b96e11e139f63fc2b679b507bbd915
Vladimir Kononovich <newinferno@gmail.com>
2023-07-15 21:36:56 +0300

    Updated grpc.

--- build_proto_68k.bat
+++ build_proto_68k.bat
@@ -1,6 +1,6 @@
 @echo off
 
-protoc -I ./proto --grpc_out=./proto --plugin=protoc-gen-grpc=c:\dev\vcpkg\packages\grpc_x64-windows\tools\grpc\grpc_cpp_plugin.exe ./proto/debug_proto_68k.proto
-protoc -I ./proto --cpp_out=./proto ./proto/debug_proto_68k.proto
+f:\dev\vcpkg\installed\x64-windows-static\tools\protobuf\protoc -I ./proto --grpc_out=./proto --plugin=protoc-gen-grpc=f:\dev\vcpkg\installed\x64-windows\tools\grpc\grpc_cpp_plugin.exe ./proto/debug_proto_68k.proto
+f:\dev\vcpkg\installed\x64-windows-static\tools\protobuf\protoc -I ./proto --cpp_out=./proto ./proto/debug_proto_68k.proto
 
 pause
