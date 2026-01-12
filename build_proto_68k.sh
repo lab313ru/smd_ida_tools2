ac3a74bd73b7549e4ca8668b1ee621df99cca48e
Vladimir Kononovich <newinferno@gmail.com>
2026-01-12 18:41:16 +0300

    Updated compilation in Linux for IDA SDK 9.2

--- build_proto_68k.sh
+++ build_proto_68k.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 protoc -I ./proto --grpc_out=./proto --plugin=protoc-gen-grpc=/usr/bin/grpc_cpp_plugin ./proto/debug_proto_68k.proto
-protoc -I ./proto --cpp_out=./proto ./proto/debug_proto_68k.proto
+protoc -I ./proto --cpp_out=./proto                                                    ./proto/debug_proto_68k.proto
 
--- compile_loaders.sh
+++ compile_loaders.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export IDA_SDK=~/ida-sdk/src
+
 mkdir -p ./linux_loaders
 g++ -D__LINUX__ -D__IDP__ -D__EA64__ -D__X64__ -O3 -shared -fPIC -o ./linux_loaders/sega_roms_ldr.so -I$IDA_SDK/include/ -L$IDA_SDK/lib/x64_linux_gcc_64/ ./sega_roms_loader/sega_roms_ldr.cpp -lida
-g++ -D__LINUX__ -D__IDP__ -D__EA64__ -D__X64__ -O3 -shared -fPIC -o ./linux_loaders/z80_loader.so -I$IDA_SDK/include/ -L$IDA_SDK/lib/x64_linux_gcc_64/ ./z80_loader/z80_loader.cpp -lida
+g++ -D__LINUX__ -D__IDP__ -D__EA64__ -D__X64__ -O3 -shared -fPIC -o ./linux_loaders/z80_loader.so    -I$IDA_SDK/include/ -L$IDA_SDK/lib/x64_linux_gcc_64/ ./z80_loader/z80_loader.cpp          -lida
